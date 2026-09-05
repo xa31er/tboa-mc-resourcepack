1630b5a48b753a5fd3086a1137b21163700d4880
xa31er <139361667+xa31er@users.noreply.github.com>
2026-09-05 17:46:42 +0200

    Added windows support

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Update
 - Make your changes
-- Run sha1sum.sh
+- Run zipsum.sh or zipsum.bat
 - Push
 - In minecraft, /resourcepack set [hash from sha1sum.txt] https://github.com/xa31er/tboa-mc-resourcepack/raw/refs/heads/main/rp.zip
---
+++ zipsum.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM Compress
+tar -a -u -f "rp.zip" "rp"
+
+SET "FILENAME=rp.zip"
+SET "FILE_HASH="
+
+FOR /F "skip=1 delims=" %%H IN ('CertUtil -hashfile "%FILENAME%"') DO (
+    SET "FILE_HASH=%%H"
+    GOTO :HashFound
+)
+
+:HashFound
+REM The hash from CertUtil contains spaces, so we remove them.
+SET "FILE_HASH=%FILE_HASH: =%"
+
+ECHO %FILE_HASH% > sha1sum.txt
